89e6482bd495846e59eef4429bae353e36a7b84b
dogtopus <dogtopus@users.noreply.github.com>
2022-08-15 00:18:59 -0300

    Implement a hack to get 8-byte aligned allocator

--- newlib/libc/sys/muteki/malloc.c
+++ newlib/libc/sys/muteki/malloc.c
@@ -14,13 +14,63 @@
 #include <_ansi.h>
 #include <errno.h>
 #include <reent.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <malloc.h>
 
 #include <muteki/memory.h>
 
+typedef struct {
+    size_t usable_size;
+    void *raw_ptr;
+} __mchx_t;
+
+static const size_t __OVER_ALLOC_SIZE = 4 + sizeof(__mchx_t);
+
 /**
- * @brief Redirects _malloc_r to lmalloc.
+ * @brief A hack that fixes allocator alignment by adding an extra header to allocated memchunk.
+ *
+ * Besta allocator is 4-bytes aligned but EABI requires 8-bytes aligned allocations. We can use compiler directives to cover this up but it might introduce performance penalty. This essentially over-allocates the memory and makes sure that there are at least 8 bytes available for us to store the original pointer and the allocation size so we don't have to resort to using the memchunk header to determine whether we're at the original pointer or not.
+ *
+ * @param q Unaligned pointer given by Besta lmalloc().
+ * @param size Usable size of the allocation.
+ * @return Pointer that aligns to 8-bytes.
+ */
+static inline void *__mchx_format(void *q, size_t size) {
+    uintptr_t pp = ((((uintptr_t) q) + __OVER_ALLOC_SIZE) & (~((uintptr_t) 7u)));
+    __mchx_t *mchx = (__mchx_t *) (pp - sizeof(__mchx_t));
+    mchx->raw_ptr = q;
+    mchx->usable_size = size;
+    return (void *) pp;
+}
+
+/**
+ * @brief Read size value from an mchx header.
+ *
+ * @param p Pointer produced by __mchx_format().
+ * @return Size of usable memory.
+ */
+static inline size_t __mchx_get_size(void *p) {
+    uintptr_t pp = (uintptr_t) p;
+    __mchx_t *mchx = (__mchx_t *) (pp - sizeof(__mchx_t));
+    return mchx->usable_size;
+}
+
+/**
+ * @brief Read raw pointer value from an mchx header.
+ *
+ * @param p Pointer produced by __mchx_format().
+ * @return The original unaligned pointer.
+ */
+static inline void *__mchx_get_raw(void *p) {
+    uintptr_t pp = (uintptr_t) p;
+    __mchx_t *mchx = (__mchx_t *) (pp - sizeof(__mchx_t));
+    return mchx->raw_ptr;
+}
+
+/**
+ * @brief Allocate and format mchx memchunk.
  *
  * Sets @p errno to @p ENOMEM if lmalloc() returns a NULL.
  *
@@ -29,17 +79,20 @@
  * @return Pointer to allocated memory, or NULL if allocation fails.
  */
 void *_malloc_r(struct _reent *r, size_t size) {
-    void *p = lmalloc(size);
-    if (p == NULL) {
+    void *q = lmalloc(size + __OVER_ALLOC_SIZE);
+
+    if (q == NULL) {
         r->_errno = ENOMEM;
+        return NULL;
     }
-    return p;
+
+    return __mchx_format(q, size);
 }
 
 /**
- * @brief Redirects _calloc_r to lcalloc.
+ * @brief Allocate and clear memory.
  *
- * Sets @p errno to @p ENOMEM if lcalloc() returns a NULL.
+ * Sets @p errno to @p ENOMEM if _malloc_r() returns a NULL.
  *
  * @param r Newlib reentrant context.
  * @param nmemb Data unit size.
@@ -47,39 +100,53 @@ void *_malloc_r(struct _reent *r, size_t size) {
  * @return Pointer to allocated memory, or NULL if allocation fails.
  */
 void *_calloc_r(struct _reent *r, size_t nmemb, size_t size) {
-    void *p = lcalloc(nmemb, size);
+    void *p = _malloc_r(r, nmemb * size);
+
     if (p == NULL) {
         r->_errno = ENOMEM;
+        return NULL;
     }
+
+    memset(p, 0, nmemb * size);
     return p;
 }
 
 /**
- * @brief Redirects _realloc_r to lrealloc.
+ * @brief Reallocate a mchx allocation.
  *
- * Sets @p errno to @p ENOMEM if lrealloc() returns a NULL.
+ * Sets @p errno to @p ENOMEM if _malloc_r() fails.
  *
  * @param r Newlib reentrant context.
- * @param ptr Pointer to previously allocated memory.
+ * @param ptr Pointer to previously allocated memory with mchx.
  * @param size Size of the new memory.
  * @return Pointer to the new allocated memory, or NULL if allocation fails.
  */
 void *_realloc_r(struct _reent *r, void *ptr, size_t size) {
-    void *p = lrealloc(ptr, size);
+    size_t orig_size = __mchx_get_size(ptr);
+    size_t copy_size = (size <= orig_size) ? size : orig_size;
+
+    void *p = _malloc_r(r, size);
     if (p == NULL) {
         r->_errno = ENOMEM;
+        return NULL;
     }
+
+    memcpy(p, ptr, copy_size);
+    _free_r(r, ptr);
     return p;
 }
 
 /**
- * @brief Redirects _free_r to _lfree.
+ * @brief Free a mchx memchunk.
  *
  * @param r Newlib reentrant context.
  * @param ptr Pointer to previously allocated memory.
  */
 void _free_r(struct _reent *r, void *ptr) {
-    _lfree(ptr);
+    if (ptr == NULL) {
+        return;
+    }
+    _lfree(__mchx_get_raw(ptr));
 }
 
 /* Below are copied from newlib's reentrent to C standard functions. */
