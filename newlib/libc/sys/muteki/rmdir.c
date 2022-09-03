f7dbcfda2ab485648a0a7ab001620b577cc65802
dogtopus <dogtopus@users.noreply.github.com>
2022-09-03 19:49:30 -0300

    Remove _rmdir_r as it seems unnecessary

--- newlib/libc/sys/muteki/rmdir.c
+++ newlib/libc/sys/muteki/rmdir.c
@@ -9,23 +9,19 @@
 #include "mutekishims_utils.h"
 #include "nowide.h"
 
-int _rmdir_r(struct _reent *r, const char *path) {
-    UTF16 *wpath = __nowide_prep_path_for_syscall_r(r, path);
+int rmdir(const char *path) {
+    UTF16 *wpath = __nowide_prep_path_for_syscall_r(_REENT, path);
     if (!wpath) {
         return -1;
     }
 
     if (_wrmdir(wpath) < 0) {
         free(wpath);
-        _REENT_ERRNO(r) = __muteki_kerrno_to_errno(_GetLastError());
+        errno = __muteki_kerrno_to_errno(_GetLastError());
         return -1;
     }
 
     free(wpath);
-    _REENT_ERRNO(r) = 0;
+    errno = 0;
     return 0;
-}
-
-int rmdir(const char *path) {
-    return _rmdir_r(_REENT, path);
 }
