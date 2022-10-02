098d880f1e4ca20327efd66d683b431b223ec4f0
dogtopus <dogtopus@users.noreply.github.com>
2022-10-02 15:05:39 -0300

    dirent.h: Fix libstdc++ compilation

--- newlib/libc/sys/muteki/sys/dirent.h
+++ newlib/libc/sys/muteki/sys/dirent.h
@@ -45,11 +45,15 @@ DEALINGS IN THE SOFTWARE.
 #define _DIRENT_HAVE_D_NAMLEN
 #endif
 
+/* Some of these are needed by libstdc++. */
 #define DT_UNKNOWN 0
+#define DT_FIFO 1
 #define DT_CHR 2
 #define DT_DIR 4
 #define DT_BLK 6
 #define DT_REG 8
+#define DT_LNK 10
+#define DT_SOCK 12
 
 /* Roughly based on the BSD dirent with added fields. */
 struct dirent {
