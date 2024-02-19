4695541af8433a1b0c60ffb3cfd122c711891af0
dogtopus <dogtopus@users.noreply.github.com>
2024-02-19 12:37:47 -0400

    Add extern "C" to heap tracer header

--- newlib/libc/sys/muteki/sys/heaptracer.h
+++ newlib/libc/sys/muteki/sys/heaptracer.h
@@ -1,9 +1,17 @@
 #ifndef __SYS_HEAPTRACER_H__
 #define __SYS_HEAPTRACER_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdbool.h>
 
 extern bool heaptracer_start();
 extern bool heaptracer_stop();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
